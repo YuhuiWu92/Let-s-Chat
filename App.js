19e2fc43e305f0758252365209ce1830d5010cc5
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-04 23:06:55 +0200

    set up

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+  },
+  box1: {
+    flex: 1,
+    backgroundColor: "pink",
+  },
+
+  box2: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+
+  box3: {
+    flex: 1,
+    backgroundColor: "black",
   },
 });
