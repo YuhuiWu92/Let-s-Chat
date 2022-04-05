9e84fef66ca0505bca2d20a247389075c0a15832
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-05 16:28:00 +0200

    update setup for navegation

--- App.js
+++ App.js
@@ -8,6 +8,18 @@ import {
   Alert,
   ScrollView,
 } from "react-native";
+// import the screens
+import Screen1 from "./components/Screen1";
+import Screen2 from "./components/Screen2";
+// import react native gesture handler
+import "react-native-gesture-handler";
+
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Create the navigator
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
   constructor(props) {
