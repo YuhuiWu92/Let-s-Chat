eba2400d0896816dc0ce31e74826eca817fe5a9e
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-04-05 18:09:09 +0200

    update navigation

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Button,
-  Alert,
-  ScrollView,
-} from "react-native";
+
 // import the screens
 import Screen1 from "./components/Screen1";
 import Screen2 from "./components/Screen2";
@@ -21,36 +13,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 // Create the navigator
 const Stack = createStackNavigator();
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: "" };
-  }
-  alertMyText(input = []) {
-    Alert.alert(input.text);
-  }
+export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <ScrollView>
-          <Text style={{ fontSize: 110 }}>
-            This text is so big! And so long! You have to scroll!
-          </Text>
-        </ScrollView>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-          placeholder="Type here ..."
-        />
-        <Text>You wrote: {this.state.text}</Text>
-        <Button
-          onPress={() => {
-            this.alertMyText({ text: this.state.text });
-          }}
-          title="Press Me"
-        />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Screen1">
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
